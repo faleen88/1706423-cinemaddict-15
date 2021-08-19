a15674e0b7ab5ff40660dbe77179ec08249944ad
Elena <enafukin@gmail.com>
2021-08-19 21:18:09 +0400

    Добавляет наследование простым компонентам

--- src/view/cards-lists.js
+++ src/view/cards-lists.js
@@ -1,4 +1,4 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
 
 const createCardsListsTemplate = () => (
   `<section class="films">
@@ -22,24 +22,8 @@ const createCardsListsTemplate = () => (
   </section>`
 );
 
-export default class CardsLists {
-  constructor() {
-    this._element = null;
-  }
-
+export default class CardsLists extends AbstractView {
   getTemplate() {
     return createCardsListsTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
