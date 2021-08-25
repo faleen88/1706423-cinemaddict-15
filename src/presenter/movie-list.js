eed1b2525d8427e5b96454156894aa49041cc7fb
Elena <enafukin@gmail.com>
2021-08-25 17:15:50 +0400

    Отрефакторит логику кнопки допоказа

--- src/presenter/movie-list.js
+++ src/presenter/movie-list.js
@@ -12,12 +12,15 @@ const CARD_COUNT_PER_STEP = 5;
 export default class MovieList {
   constructor(movieListContainer) {
     this._movieListContainer = movieListContainer;
+    this._renderedCardCount = CARD_COUNT_PER_STEP;
 
     this._cardsListsComponent = new CardsListsView();
     this._cardsListComponent = new CardsListView();
     this._filmListComponent = new FilmListView();
     this._noCardComponent = new NoCardView();
-    this._showMoreButtoComponent = new ShowMoreButtonView();
+    this._showMoreButtonComponent = new ShowMoreButtonView();
+
+    this._handleShowMoreButtonClick = this._handleShowMoreButtonClick.bind(this);
   }
 
   init(cardsList) {
@@ -74,24 +77,19 @@ export default class MovieList {
     render(this._cardsListComponent, this._noCardComponent);
   }
 
+  _handleShowMoreButtonClick() {
+    this._renderCards(this._renderedCardCount, this._renderedCardCount + CARD_COUNT_PER_STEP);
+    this._renderedCardCount += CARD_COUNT_PER_STEP;
+
+    if (this._renderedCardCount >= this._cardsList.length) {
+      remove(this._showMoreButtonComponent);
+    }
+  }
+
   _renderShowMoreButton() {
-    let renderedCardCount = CARD_COUNT_PER_STEP;
+    render(this._cardsListComponent, this._showMoreButtonComponent);
 
-    const showMoreButtonComponent = new ShowMoreButtonView();
-
-    render(this._cardsListComponent, showMoreButtonComponent);
-
-    showMoreButtonComponent.setClickHandler(() => {
-      this._cardsList
-        .slice(renderedCardCount, renderedCardCount + CARD_COUNT_PER_STEP)
-        .forEach((card) => this._renderCard(card));
-
-      renderedCardCount += CARD_COUNT_PER_STEP;
-
-      if (renderedCardCount >= this._cardsList.length) {
-        remove(showMoreButtonComponent);
-      }
-    });
+    this._showMoreButtonComponent.setClickHandler(this._handleShowMoreButtonClick);
   }
 
   _renderCardList() {
