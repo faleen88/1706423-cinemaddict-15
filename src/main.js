3b03038845f85982ca4bc0e66ddd3558d2dcba28
Elena <enafukin@gmail.com>
2021-08-27 13:05:29 +0400

    Добавляет сортировку карточек фильмов

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import UserRankView from './view/user-rank.js';
 import NavigationView from './view/navigation.js';
-import SortView from './view/sort.js';
 import {generateCard} from './mock/card.js';
 import {generateFilter} from './mock/filter.js';
 import {render} from './utils/render.js';
 import MovieListPresenter from './presenter/movie-list.js';
 
 const CARD_COUNT = 15;
-//const CARD_COUNT_EXTRA = 2;
 
 const films = new Array(CARD_COUNT).fill().map(generateCard);
 const filters = generateFilter(films);
@@ -15,18 +13,10 @@ const filters = generateFilter(films);
 const siteBody = document.querySelector('body');
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
-/*
-filmsListsExtra.forEach((list) => {
-  const filmsListContainerExtra = list.querySelector('.films-list__container');
-  for (let i = 0; i < CARD_COUNT_EXTRA; i++) {
-    renderCardFilm(filmsListContainerExtra, cardsList[i]);
-  }
-});
-*/
+
 const movieListPresenter = new MovieListPresenter(siteMainElement, siteBody);
 
 render(siteHeaderElement, new UserRankView());
 render(siteMainElement, new NavigationView(filters));
-render(siteMainElement, new SortView());
 
 movieListPresenter.init(films);
--- src/presenter/movie-list.js
+++ src/presenter/movie-list.js
@@ -3,9 +3,12 @@ import CardsListView from '../view/cards-list';
 import FilmListView from '../view/films-container.js';
 import ShowMoreButtonView from '../view/show-more.js';
 import NoCardView from '../view/no-card.js';
+import SortView from '../view/sort.js';
 import {render, remove} from '../utils/render.js';
 import {updateItem} from '../utils/common.js';
 import MoviePresenter from './movie.js';
+import dayjs from 'dayjs';
+import {SortType} from '../view/sort.js';
 
 const CARD_COUNT_PER_STEP = 5;
 
@@ -15,20 +18,26 @@ export default class MovieList {
     this._renderedCardCount = CARD_COUNT_PER_STEP;
     this._siteContainer = siteContainer;
     this._moviePresenter = new Map();
+    this._currentSortType = SortType.DEFAULT;
 
     this._cardsListsComponent = new CardsListsView();
     this._cardsListComponent = new CardsListView();
     this._filmListComponent = new FilmListView();
     this._noCardComponent = new NoCardView();
     this._showMoreButtonComponent = new ShowMoreButtonView();
+    this._sortComponent = new SortView();
 
     this._handleCardChange = this._handleCardChange.bind(this);
     this._handleModeChange = this._handleModeChange.bind(this);
     this._handleShowMoreButtonClick = this._handleShowMoreButtonClick.bind(this);
+    this._handleSortTypeChange = this._handleSortTypeChange.bind(this);
   }
 
   init(cardsList) {
     this._cardsList = cardsList.slice();
+    this._sourcedCardsList = cardsList.slice();
+
+    this._renderSort();
 
     render(this._movieListContainer, this._cardsListsComponent);
     render(this._cardsListsComponent, this._cardsListComponent);
@@ -43,7 +52,38 @@ export default class MovieList {
 
   _handleCardChange(updatedCard) {
     this._cardsList = updateItem(this._cardsList, updatedCard);
+    this._sourcedCardsList = updateItem(this._sourcedCardsList, updatedCard);
     this._moviePresenter.get(updatedCard.id).init(updatedCard);
+  }
+
+  _sortCards(sortType) {
+    switch (sortType) {
+      case SortType.DATE:
+        this._cardsList.sort((cardA, cardB) => dayjs(cardB.releaseDate).diff(dayjs(cardA.releaseDate)));
+        break;
+      case SortType.RATING:
+        this._cardsList.sort((cardA, cardB) => cardB.rating - cardA.rating);
+        break;
+      default:
+        this._cardsList = this._sourcedCardsList.slice();
+    }
+
+    this._currentSortType = sortType;
+  }
+
+  _handleSortTypeChange(sortType) {
+    if (this._currentSortType === sortType) {
+      return;
+    }
+
+    this._sortCards(sortType);
+    this._clearCardList();
+    this._renderCardList();
+  }
+
+  _renderSort() {
+    render(this._movieListContainer, this._sortComponent);
+    this._sortComponent.setSortTypeChangeHandler(this._handleSortTypeChange);
   }
 
   _renderCard(card) {
